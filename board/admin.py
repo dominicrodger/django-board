e0d430cf8a41babafc9df775b764ca952f7f72e8
Dominic Rodger <dominicrodger+github@gmail.com>
2011-10-04 23:05:15 +0100

    Closes #1. Adding TinyMCE support (as a requirement).

--- board/admin.py
+++ board/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from board.models import BoardMember
 from sorl.thumbnail.admin import AdminImageMixin
+from tinymce.widgets import TinyMCE
 
 class BoardMemberAdmin(AdminImageMixin, admin.ModelAdmin):
     list_display = ('forename', 'surname', 'admin_thumbnail', 'position', 'email',)
     search_fields = ('forename', 'surname', 'position', 'email', 'biography',)
+
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        if db_field.name in ('biography',):
+            return db_field.formfield(widget=TinyMCE(
+                attrs={'cols': 80, 'rows': 30},
+            ))
+        return super(BoardMemberAdmin, self).formfield_for_dbfield(db_field, **kwargs)
+
 admin.site.register(BoardMember, BoardMemberAdmin)
