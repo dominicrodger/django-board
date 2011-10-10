40acc52cc065a1b324a6c0dfe987354847fa0b13
Dominic Rodger <dominicrodger+github@gmail.com>
2011-10-10 18:30:41 +0100

    Making django-board easier to install with PIP.

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+import board
 
 setup(
     name='django-board',
-    version='0.1.0',
+    version=board.__version__,
     description='A Django app for managing an organisation\'s board members page.',
     long_description=open('README.md').read(),
     author='Dominic Rodger',
     author_email='internet@dominicrodger.com',
     url='http://github.com/dominicrodger/django-board',
     license='BSD',
-    packages=['board'],
+    packages=find_packages(),
     include_package_data=True,
     package_data={'': ['README.md']},
     zip_safe=False,
