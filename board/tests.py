c4ba52adafb88d5ea333f61def0039b13a3e7b1e
Dominic Rodger <dom@DMR-Ubuntu.(none)>
2012-04-23 05:48:51 +0100

    Removing another unused import.

--- board/tests.py
+++ board/tests.py
@@ -1,6 +1,5 @@
 from django.core.urlresolvers import reverse
 from django.test import TestCase
-from django.test.client import Client
 from board.models import BoardMember
 
 class BoardMemberTestCase(TestCase):
