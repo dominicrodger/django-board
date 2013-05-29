7033d9e938a2de740b51c53dca8959a23f525d92
Dominic Rodger <dominicrodger+github@gmail.com>
2013-05-29 17:53:01 +0100

    Test with tox

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         'License :: OSI Approved :: BSD License',
     ],
     tests_require=(
-        'django-setuptest==0.1.2',
+        'django-setuptest==0.1.3',
         'factory_boy==1.2.0',
     ),
     test_suite='setuptest.setuptest.SetupTestSuite',
